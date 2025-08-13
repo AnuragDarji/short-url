6b483a5f6fd5e46b286c1ae61930168856fb857e
AnuragDarji <anuragdarji29@gmail.com>
2025-08-13 17:01:31 +0530

    update

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-const express = require("express");
-const urlRoute = require("./routes/url");
-const { connectToMongoDB } = require("./connect");
-const URL = require("./models/url");
-require("dotenv").config();
-const mongourl = process.env.MONGO_URL;
+const app = require("./app");
 
-const app = express();
-const PORT = 8001;
-
-connectToMongoDB(mongourl).then(() =>
-  console.log("Mongodb connected")
-);
-app.use(express.json());
-app.use("/url", urlRoute);
-
-app.get("/:shortId", async (req, res) => {
-  const shortId = req.params.shortId;
-  const entry = await URL.findOneAndUpdate(
-    { shortId },
-    {
-      $push: {
-        visitHistroy: { timestamp: Date.now() },
-      },
-    }
-  );
-  res.redirect(entry.redirectURL);
-});
-
-app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
+module.exports = app;
